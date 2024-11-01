2ad50c368210cf9721d693b9ea13651f32f4e70b
fathimathjazeera <jezijazeera@gmail.com>
2024-11-01 19:38:32 +0530

    project setup,auth layout

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom"
+import AuthLogin from "./pages/auth/login"
+import AuthLayout from './components/auth/layout'
+import AuthRegister from "./pages/auth/register"
+
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col overflow-hidden bg-white">
+
+
+<Routes>
+  <Route path="/auth" element={<AuthLayout/>}>
+<Route path="login" element={<AuthLogin />}/>
+<Route path="register" element={<AuthRegister />}/>
+  </Route>
+</Routes>
+
+      </div>
+    </>
+  )
+}
+
+export default App
