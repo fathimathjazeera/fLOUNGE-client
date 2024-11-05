ad00d38f2ca3cdc84c0649071cfaae5918a44dad
fathimathjazeera <jezijazeera@gmail.com>
2024-11-05 14:35:45 +0530

    check auth added

---
+++ src/components/common/form.jsx
@@ -0,0 +1,10 @@
+
+
+
+function CommonForm(){
+    return(
+        <form action="">
+            
+        </form>
+    )
+}
---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+export const registerFormControls = [
+    {
+        name:'userName',
+        label:'User Name',
+        placeholder:'Enter your user name',
+        componentType:'input',
+        type:'text'
+    }
+]
